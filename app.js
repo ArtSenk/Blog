bbc0152a757ffa42d8036e65f0a54ab8a6cf2ad7
PC <art.senkov.yom@gmail.com>
2016-12-25 13:48:41 +0300

    add db obj

--- app.js
+++ app.js
@@ -9,12 +9,27 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
+// MONGOOSE CONFIG
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+});
+var Blog = mongoose.model("Blog", blogSchema);
+
 app.get("/", function (req, res) {
     res.render("landing");
 });
 
 app.get("/blogs", function (req, res) {
-    res.render("index");
+    Blog.find({}, function (err, blogs) {
+        if (err) {
+            console.log("ERROR!");
+        } else {
+            res.render("index", {blogs: blogs});
+        }
+    });
 });
 
 app.listen(process.env.PORT || 3000, process.env.IP);
