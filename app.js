c4fa72e19c9a57908c7ac9df0b5804ada35728b3
PC <art.senkov.yom@gmail.com>
2016-12-27 23:07:58 +0300

    unregister logic

--- app.js
+++ app.js
@@ -57,25 +57,6 @@ app.get("/blogs/:id", function (req, res) {
     });
 });
 
-// AUTH ROUTES
-// show register form
-app.get("/register", function (req, res) {
-    res.render("register");
-});
-// handle sign up logic
-app.post("/register", function (req, res) {
-    var newUser = new User({username: req.body.username});
-    User.register(newUser, req.body.password, function (err, user) {
-        if (err) {
-            console.log(err);
-            return res.render("register");
-        }
-        passport.authenticate("local")(req, res, function () {
-            res.redirect("/blogs")
-        });
-    });
-});
-
 // show login form
 app.get("/login", function (req, res) {
     res.render("login");
