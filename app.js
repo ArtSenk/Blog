c9bbdf0ae5bcf0fd37d79672b6efcfbd7806d4a9
PC <art.senkov.yom@gmail.com>
2016-12-27 22:39:40 +0300

    add logic authenticated and register/login/logout logic

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-var bodyParser = require("body-parser"),
-    mongoose   = require("mongoose"),
-    express    = require("express"),
-    app        = express();
+var express       = require("express"),
+    app           = express(),
+    bodyParser    = require("body-parser"),
+    mongoose      = require("mongoose"),
+    passport      = require("passport"),
+    LocalStrategy = require("passport-local"),
+    User          = require("./models/usr");
 
 mongoose.connect(process.env.DATABASEURL);
 
@@ -17,6 +20,18 @@ var blogSchema = new mongoose.Schema({
     created: {type: Date, default: Date.now}
 });
 var Blog = mongoose.model("Blog", blogSchema);
+
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "Fucking secret",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function (req, res) {
     res.render("landing");
@@ -40,6 +55,43 @@ app.get("/blogs/:id", function (req, res) {
             res.render("show", {blog: foundBlog});
         }
     });
+});
+
+// AUTH ROUTES
+// show register form
+app.get("/register", function (req, res) {
+    res.render("register");
+});
+// handle sign up logic
+app.post("/register", function (req, res) {
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function (err, user) {
+        if (err) {
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function () {
+            res.redirect("/blogs")
+        });
+    });
+});
+
+// show login form
+app.get("/login", function (req, res) {
+    res.render("login");
+});
+// handling login logic
+app.post("/login", passport.authenticate("local",
+    {
+        successRedirect: "/blogs",
+        failureRedirect: "/login"
+    }), function (req, res) {
+});
+
+// logic route
+app.get("/logout", function (req, res) {
+    req.logout();
+    res.redirect("/blogs");
 });
 
 app.listen(process.env.PORT || 3000, process.env.IP);
