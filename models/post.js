48627760585cee9fb12625e98e55838126242603
PC <art.senkov.yom@gmail.com>
2016-12-31 00:07:16 +0300

    add comment in postSchema

--- models/post.js
+++ models/post.js
@@ -4,7 +4,13 @@ var postSchema = new mongoose.Schema({
     name: String,
     image: String,
     description: String,
-    created: {type: Date, default: Date.now}
+    created: {type: Date, default: Date.now},
+    comments: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Comment"
+        }
+    ]
 });
 
 module.exports = mongoose.model("Post", postSchema);
