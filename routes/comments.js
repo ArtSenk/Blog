88c8e1e6f4914b57f9e2f22461436b02068d3c27
PC <art.senkov.yom@gmail.com>
2017-01-01 23:43:12 +0300

    add username and id to comment

--- routes/comments.js
+++ routes/comments.js
@@ -27,8 +27,14 @@ router.post("/", function (req, res) {
                 if (err) {
                     console.log(err);
                 } else {
+                    // add username and id to comment
+                    comment.author.id = req.user._id;
+                    comment.author.username = req.user.username;
+                    // save comment
+                    comment.save();
                     post.comments.push(comment);
                     post.save();
+                    console.log(comment);
                     res.redirect('/posts/' + post._id);
                 }
             });
