0d01f7e9993e1fd6dd242a6ce90a4e9fc8b6978c
PC <art.senkov.yom@gmail.com>
2017-01-05 18:19:14 +0300

    edit(get) & update(put) post routes

--- routes/posts.js
+++ routes/posts.js
@@ -2,8 +2,9 @@ var express = require("express");
 var router = express.Router();
 var Post = require("../models/post");
 
-// Show all posts
+//INDEX - show all posts
 router.get("/", function (req, res) {
+    // Get all posts from DB
     Post.find({}, function (err, allPosts) {
         if (err) {
             console.log(err);
@@ -13,8 +14,9 @@ router.get("/", function (req, res) {
     });
 });
 
-// Add new post to DB
+//CREATE - add new post to DB
 router.post("/", isLoggedIn, function (req, res) {
+    // get data from form and add to posts array
     var name = req.body.name;
     var image = req.body.image;
     var desc = req.body.description;
@@ -23,22 +25,24 @@ router.post("/", isLoggedIn, function (req, res) {
         username: req.user.username
     };
     var newPost = {name: name, image: image, description: desc, author: author};
-
+    // Create a new post and save to DB
     Post.create(newPost, function (err, newlyCreated) {
         if (err) {
             console.log(err);
         } else {
+            //redirect back to posts page
             console.log(newlyCreated);
             res.redirect("/posts");
         }
     });
 });
 
+//NEW - show form to create new post
 router.get("/new", isLoggedIn, function (req, res) {
     res.render("posts/new");
 });
 
-// Shows more info about one post
+//Show - shows more info about one post
 router.get("/:id", function (req, res) {
     Post.findById(req.params.id).populate("comments").exec(function (err, foundPost) {
         if (err) {
@@ -50,9 +54,33 @@ router.get("/:id", function (req, res) {
     });
 });
 
+// EDIT POST ROUTE
+router.get("/:id/edit", function (req, res) {
+    Post.findById(req.params.id, function (err, foundPost) {
+        if (err) {
+            res.redirect("/posts");
+        } else {
+            res.render("posts/edit", {post: foundPost});
+        }
+    });
+});
+
+// UPDATE POST ROUTE
+router.put("/:id", function (req, res) {
+    // find and update the current post
+    Post.findByIdAndUpdate(req.params.id, req.body.post, function (err, updatePost) {
+        if (err) {
+            res.redirect("/posts");
+        } else {
+            // redirect show page
+            res.redirect("/posts/" + req.params.id);
+        }
+    });
+});
+
 // middleware
 function isLoggedIn(req, res, next){
-    if(req.isAuthenticated()){
+    if (req.isAuthenticated()) {
         return next();
     }
     res.redirect("/login");
