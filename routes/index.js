f6a91b3de3ba7f8bdf79ddcc5076478f9dced5ba
PC <art.senkov.yom@gmail.com>
2017-01-12 00:45:34 +0300

    flash -> sign up

--- routes/index.js
+++ routes/index.js
@@ -19,10 +19,11 @@ router.post("/register", function (req, res) {
     var newUser = new User({username: req.body.username});
     User.register(newUser, req.body.password, function (err, user) {
         if (err) {
-            console.log(err);
+            req.flash("error", err.message);
             return res.render("register");
         }
         passport.authenticate("local")(req, res, function () {
+            req.flash("success", "Welcome " + user.username);
             res.redirect("/posts");
         });
     });
@@ -47,13 +48,5 @@ router.get("/logout", function (req, res) {
     req.flash("success", "Logged you out");
     res.redirect("/posts");
 });
-
-// middleware
-function isLoggedIn(req, res, next){
-    if(req.isAuthenticated()){
-        return next();
-    }
-    res.redirect("/login");
-}
 
 module.exports = router;
