66b476fa5597fad1db8d12522f1a6e442b61bdbf
PC <art.senkov.yom@gmail.com>
2017-01-11 23:01:40 +0300

    add flash in .isLoggedIn

--- middleware/index.js
+++ middleware/index.js
@@ -46,6 +46,7 @@ middlewareObj.isLoggedIn = function (req, res, next) {
     if (req.isAuthenticated()) {
         return next();
     }
+    req.flash("error", "Please Login first");
     res.redirect("/login");
 };
 
