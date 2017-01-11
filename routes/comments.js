8b78ef8e37f9ba4476b2779e7ac0bde39db2f68d
PC <art.senkov.yom@gmail.com>
2017-01-12 00:44:10 +0300

    flash for comments

--- routes/comments.js
+++ routes/comments.js
@@ -26,6 +26,7 @@ router.post("/", function (req, res) {
         } else {
             Comment.create(req.body.comment, function (err, comment) {
                 if (err) {
+                    req.flash("error", "Something went wrong");
                     console.log(err);
                 } else {
                     // add username and id to comment
@@ -36,6 +37,7 @@ router.post("/", function (req, res) {
                     post.comments.push(comment);
                     post.save();
                     console.log(comment);
+                    req.flash("success", "Successfully added comment");
                     res.redirect('/posts/' + post._id);
                 }
             });
@@ -71,6 +73,7 @@ router.delete("/:comment_id", middleware.checkCommentOwnership, function (req, r
         if (err) {
             res.redirect("back");
         } else {
+            req.flash("success", "Comment deleted");
             res.redirect("/posts/" + req.params.id);
         }
     });
