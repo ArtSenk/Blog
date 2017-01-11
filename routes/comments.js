640415798fc8beca69e8052ecfcb5b59d1b432ad
PC <art.senkov.yom@gmail.com>
2017-01-11 16:33:27 +0300

    Comment destroy route

--- routes/comments.js
+++ routes/comments.js
@@ -64,6 +64,17 @@ router.put("/:comment_id", function (req, res) {
     });
 });
 
+// COMMENT DESTROY ROUTE
+router.delete("/:comment_id", function (req, res) {
+    Comment.findByIdAndRemove(req.params.comment_id, function (err) {
+        if (err) {
+            res.redirect("back");
+        } else {
+            res.redirect("/posts/" + req.params.id);
+        }
+    });
+});
+
 // middleware
 function isLoggedIn(req, res, next){
     if(req.isAuthenticated()){
